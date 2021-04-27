ce89fa2a23c4ce221da50460f799e6d5f548357f
Stefan Toncu <stefan.toncu29@gmail.com>
2021-04-27 14:49:28 +0300

    end-of-file

---
+++ HackerRank/Java/Introduction/Java End-of-file/Solution.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int index = 1;
+        while (scan.hasNext()) {
+            System.out.println(index++ + " " + scan.nextLine());
+        }
+    }
+}
