0abd177c3f9d32edf3d8f1560ade740a8db6951f
Stefan Toncu <stefan.toncu29@gmail.com>
2021-04-27 10:56:55 +0300

    stdin-stdout

---
+++ HackerRank/Java/Introduction/StdinStdout/Solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(scan.nextInt());
+        }
+    }
+}
