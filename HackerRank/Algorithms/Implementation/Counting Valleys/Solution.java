5975b5bcfc8db2dd459937c0193ae80bb2333d32
Stefan Toncu <stefan.toncu29@gmail.com>
2021-05-26 15:55:50 +0300

    Counting Valleys

---
+++ HackerRank/Algorithms/Implementation/Counting Valleys/Solution.java
@@ -0,0 +1,52 @@
+// https://www.hackerrank.com/challenges/counting-valleys
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int countingValleys(int steps, String path) {
+        int height = 0, valleys = 0;
+        for (char c : path.toCharArray()) {
+            if (c == 'D') {
+                height--;
+            } else {
+                height++;
+                
+                if (height == 0) {
+                    valleys++;
+                }
+            }
+        }
+        return valleys;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int steps = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String path = bufferedReader.readLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
