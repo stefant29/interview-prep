1e30ffa3a90029b0a48fb935cf5dc14a94c7bc7b
Stefan Toncu <stefan.toncu29@gmail.com>
2021-05-27 11:13:03 +0300

    Find Digits

---
+++ HackerRank/Algorithms/Implementation/Find Digits/Solution.java
@@ -0,0 +1,56 @@
+// https://www.hackerrank.com/challenges/find-digits
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int findDigits(int n) {
+        int nCopy = n, count = 0;
+
+        while (nCopy > 0) {
+            int lastDigit = nCopy % 10;
+            if (lastDigit != 0 && n % lastDigit == 0) {
+                count++;
+            }
+            nCopy /= 10;
+        }
+        
+        return count;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int result = Result.findDigits(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
