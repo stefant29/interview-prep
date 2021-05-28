b93bc689b6a91abeca312f7f1c3823f8eb16fb7a
Stefan Toncu <stefan.toncu29@gmail.com>
2021-05-28 11:13:36 +0300

    Sequence Equation

---
+++ HackerRank/Algorithms/Implementation/Sequence Equation/Solution.java
@@ -0,0 +1,57 @@
+// https://www.hackerrank.com/challenges/permutation-equation
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static List<Integer> permutationEquation(List<Integer> p) {
+        List<Integer> result = new ArrayList<Integer>();
+        HashMap<Integer, Integer> map = new HashMap();
+
+        for (int i = 0; i < p.size(); i++) {
+            map.put(p.get(p.get(i) - 1), i+1);
+        }
+        
+        for (int i = 1; i <= p.size(); i++) {
+            result.add(map.get(i));
+        }
+
+        return result;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> p = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.permutationEquation(p);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
