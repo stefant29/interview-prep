c899b707ec36f1c9c82639b6f4da854016f0506f
Stefan Toncu <stefan.toncu29@gmail.com>
2021-05-28 11:13:56 +0300

    Viral Advertising

---
+++ HackerRank/Algorithms/Implementation/Viral Advertising/Solution.java
@@ -0,0 +1,45 @@
+// https://www.hackerrank.com/challenges/strange-advertising/problem?isFullScreen=true
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int viralAdvertising(int n) {
+        int shared = 5, liked = 2;
+        
+        for (int i = 1; i < n; i++) {
+            shared = (shared / 2) * 3;
+            liked += shared / 2;
+        }
+        
+        return liked;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.viralAdvertising(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
