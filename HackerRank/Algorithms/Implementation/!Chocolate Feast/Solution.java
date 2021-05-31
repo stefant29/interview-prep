aa02654b7c2461944e21dabbd90cd726760d382e
Stefan Toncu <stefan.toncu29@gmail.com>
2021-05-31 16:49:16 +0300

    Chocolate Feast

---
+++ HackerRank/Algorithms/Implementation/!Chocolate Feast/Solution.java
@@ -0,0 +1,62 @@
+// https://www.hackerrank.com/challenges/chocolate-feast
+\
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int chocolateFeast(int money, int cost, int turnCost) {
+        int bars = money / cost, leftovers = money / cost;
+        
+        while (leftovers >= turnCost) {
+            // add the leftovers turned into bars
+            bars += leftovers / turnCost;
+            // compute the leftovers: 
+            //  new leftovers from used bars + remaining leftovers from previous bars
+            leftovers = leftovers / turnCost + leftovers % turnCost;
+        }
+        
+        return bars;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int n = Integer.parseInt(firstMultipleInput[0]);
+
+                int c = Integer.parseInt(firstMultipleInput[1]);
+
+                int m = Integer.parseInt(firstMultipleInput[2]);
+
+                int result = Result.chocolateFeast(n, c, m);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
