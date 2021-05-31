3a6707fbbb29564df38198aa4a7db565a2527de4
Stefan Toncu <stefan.toncu29@gmail.com>
2021-05-31 16:49:43 +0300

    Beautiful Triplets

---
+++ HackerRank/Algorithms/Implementation/Beautiful Triplets/Solution.java
@@ -0,0 +1,59 @@
+// https://www.hackerrank.com/challenges/beautiful-triplets
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int beautifulTriplets(int d, List<Integer> arr) {
+        int count = 0;
+        HashMap<Integer, Integer> map = new HashMap();
+        for (int i : arr) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int index = entry.getKey();
+            if (map.containsKey(index + d) && map.containsKey(index + d * 2)) {
+                count += entry.getValue() * map.get(index + d) * map.get(index + d * 2);
+            }
+        }
+        
+        return count;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.beautifulTriplets(d, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
