cd9fe39965503d5d6b56c97f2215bcc6f1ebeaec
Stefan Toncu <stefan.toncu29@gmail.com>
2021-06-02 16:51:35 +0300

    Haloween Sale

---
+++ HackerRank/Algorithms/Implementation/Haloween Sale/Solution.java
@@ -0,0 +1,56 @@
+// https://www.hackerrank.com/challenges/halloween-sale
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int howManyGames(int price, int discount, int margin, int total) {
+        int count = 0;
+        
+        while (total >= price) {
+            count++;
+            
+            total -= price;
+            
+            price = Math.max(price - discount, margin);
+        }
+        
+        return count;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int p = Integer.parseInt(firstMultipleInput[0]);
+
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        int m = Integer.parseInt(firstMultipleInput[2]);
+
+        int s = Integer.parseInt(firstMultipleInput[3]);
+
+        int answer = Result.howManyGames(p, d, m, s);
+
+        bufferedWriter.write(String.valueOf(answer));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
