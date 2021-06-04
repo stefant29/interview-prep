1c96970cec05079319f1ffe3a180641a16b121ae
Stefan Toncu <stefan.toncu29@gmail.com>
2021-06-04 16:54:12 +0300

    Service lane

---
+++ HackerRank/Algorithms/Implementation/Service Lane/Solution.java
@@ -0,0 +1,78 @@
+// https://www.hackerrank.com/challenges/service-lane
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static List<Integer> serviceLane(int n, List<Integer> width, List<List<Integer>> testCases) {
+        List<Integer> result = new ArrayList();
+        
+        for (List<Integer> testCase : testCases) {
+            int minWidth = 3;
+        
+            for (int i = testCase.get(0); i <= testCase.get(1); i++) {
+                if (width.get(i) < minWidth) {
+                    minWidth = width.get(i);
+                }
+            }
+            
+            result.add(minWidth);
+        }
+        
+        return result;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int t = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> width = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<List<Integer>> cases = new ArrayList<>();
+
+        IntStream.range(0, t).forEach(i -> {
+            try {
+                cases.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        List<Integer> result = Result.serviceLane(n, width, cases);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
