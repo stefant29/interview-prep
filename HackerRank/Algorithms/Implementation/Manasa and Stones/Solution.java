967246b68aacf6b08236058afec06c2e45231b65
Stefan Toncu <stefan.toncu29@gmail.com>
2021-06-09 16:30:31 +0300

    Manasa and Stones

---
+++ HackerRank/Algorithms/Implementation/Manasa and Stones/Solution.java
@@ -0,0 +1,65 @@
+// https://www.hackerrank.com/challenges/manasa-and-stones
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static List<Integer> stones(int n, int a, int b) {
+        if (a == b) {
+            return Arrays.asList(new Integer[]{ a * (n-1) });
+        }
+
+        List<Integer> result = new ArrayList();
+        for (int i = 0; i < n; i++) {
+            result.add(a * i + b * (n-1-i));
+        }
+
+        Collections.sort(result);
+
+        return result;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int T = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, T).forEach(TItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int a = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int b = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> result = Result.stones(n, a, b);
+
+                bufferedWriter.write(
+                    result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                    + "\n"
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
