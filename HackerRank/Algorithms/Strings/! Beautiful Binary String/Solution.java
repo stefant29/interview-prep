3aabbff419a31e51b9346d5cfdd22a83e8c19c73
Stefan Toncu <stefan.toncu29@gmail.com>
2021-06-17 21:58:39 +0300

    Strings beautiful binary string

---
+++ HackerRank/Algorithms/Strings/! Beautiful Binary String/Solution.java
@@ -0,0 +1,50 @@
+// https://www.hackerrank.com/challenges/beautiful-binary-string
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int beautifulBinaryString(String b) {
+        // if Alice finds 010, she should replace it with 011
+        int index = b.indexOf("010");
+        int count = 0;
+        
+        while (index >= 0) {
+            count++;
+            
+            index = b.indexOf("010", index + 3);
+        }
+        
+        return count;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String b = bufferedReader.readLine();
+
+        int result = Result.beautifulBinaryString(b);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
