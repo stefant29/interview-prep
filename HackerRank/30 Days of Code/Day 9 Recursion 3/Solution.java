9064cf3d6b28572a5c1fcdda282e6bb30f6c3399
Stefan Toncu <stefan.toncu29@gmail.com>
2021-06-22 22:02:53 +0300

    30 days - day 9.

---
+++ HackerRank/30 Days of Code/Day 9 Recursion 3/Solution.java
@@ -0,0 +1,38 @@
+// https://www.hackerrank.com/challenges/30-recursion
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+    
+    public static int factorial(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        
+        return n * factorial(n-1);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.factorial(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
