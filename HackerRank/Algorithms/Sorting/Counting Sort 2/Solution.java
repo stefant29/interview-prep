845507384666241d0437715ad141e97c37b0e0cd
Stefan Toncu <stefan.toncu29@gmail.com>
2021-06-23 17:08:26 +0300

    Counting Sort 2

---
+++ HackerRank/Algorithms/Sorting/Counting Sort 2/Solution.java
@@ -0,0 +1,59 @@
+// https://www.hackerrank.com/challenges/countingsort2
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static List<Integer> countingSort(List<Integer> arr) {
+        List<Integer> result = new ArrayList();
+        int[] count = new int[100];
+        
+        for (int i : arr) {
+            count[i-1]++;
+        }
+        
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < count[i]; j++) {
+                result.add(i+1);
+            }
+        }
+        
+        return result;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.countingSort(arr);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
