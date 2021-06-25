19321988c2f4ce91a11f4d8f7394b75e8ea40740
Stefan Toncu <stefan.toncu29@gmail.com>
2021-06-25 17:10:26 +0300

    Big Sorting

---
+++ HackerRank/Algorithms/Sorting/!Big Sorting/Solution.java
@@ -0,0 +1,63 @@
+// https://www.hackerrank.com/challenges/big-sorting
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static List<String> bigSorting(List<String> unsorted) {
+        Collections.sort(unsorted, new Comparator<String>(){
+            @Override
+            public int compare(final String a, String b) {
+                if (a.length() > b.length()) {
+                    return 1;
+                } else if (a.length() < b.length()) {
+                    return -1;
+                } 
+                
+                return a.compareTo(b);
+            }
+        });
+        
+        return unsorted;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<String> unsorted = IntStream.range(0, n).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .collect(toList());
+
+        List<String> result = Result.bigSorting(unsorted);
+
+        bufferedWriter.write(
+            result.stream()
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
