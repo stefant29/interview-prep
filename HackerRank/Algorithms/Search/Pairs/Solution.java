0f3ca7a9a51bc8bacfccef2ec705c7bf14942296
Stefan Toncu <stefan.toncu29@gmail.com>
2021-07-07 17:15:43 +0300

    Pairs

---
+++ HackerRank/Algorithms/Search/Pairs/Solution.java
@@ -0,0 +1,55 @@
+// https://www.hackerrank.com/challenges/pairs
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int pairs(int k, List<Integer> arr) {
+        int countPairs = 0;
+        Set<Integer> set = new HashSet(arr);
+
+        for (int i : arr) {
+            if (set.contains(i-k)) {
+                countPairs++;
+            }
+        }
+        
+        return countPairs;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.pairs(k, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
