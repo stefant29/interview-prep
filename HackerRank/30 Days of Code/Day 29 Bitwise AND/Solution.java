a02fca6a6485982588ad5d1832a20c6dc946dfac
Stefan Toncu <stefan.toncu29@gmail.com>
2021-07-19 17:23:17 +0300

    30 Days of Code, Day 2

---
+++ HackerRank/30 Days of Code/Day 29 Bitwise AND/Solution.java
@@ -0,0 +1,53 @@
+// https://www.hackerrank.com/challenges/30-bitwise-and
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    public static int bitwiseAnd(int N, int K) {
+        int max = 0;
+        
+        for (int i = 1; i < N; i++) {
+            for (int j = i+1; j <= N; j++) {
+                int and = i & j;
+                if (and < K) {
+                    max = (int) Math.max(and, max);
+                }
+            }
+        }
+        
+        return max;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            int count = Integer.parseInt(firstMultipleInput[0]);
+
+            int lim = Integer.parseInt(firstMultipleInput[1]);
+
+            int res = Result.bitwiseAnd(count, lim);
+
+            bufferedWriter.write(String.valueOf(res));
+            bufferedWriter.newLine();
+        }
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
