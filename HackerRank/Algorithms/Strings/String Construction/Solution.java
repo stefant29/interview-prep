404ed5ee65831b22e5f0d7f9a2767601095f0398
Stefan Toncu <stefan.toncu29@gmail.com>
2021-07-30 17:27:27 +0300

    String Construction

---
+++ HackerRank/Algorithms/Strings/String Construction/Solution.java
@@ -0,0 +1,51 @@
+// https://www.hackerrank.com/challenges/string-construction
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int stringConstruction(String s) {
+        Set<Character> set = new HashSet<Character>();
+        for (char c : s.toCharArray()) {
+            set.add(c);
+        }
+        
+        return set.size();
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                int result = Result.stringConstruction(s);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
